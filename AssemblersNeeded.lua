ddff0566869d71dffa8257065a0421b37f9c3607
renderman21 <renderman21@gmail.com>
2022-12-21 12:19:38 +0800

    Added comments and stuff

--- AssemblersNeeded.lua
+++ AssemblersNeeded.lua
@@ -1,25 +1,36 @@
 require "utils"
 
+---Prints the result
+---@param amountOutput integer
+---@param itemCraftingSpeed integer
+---@param assemblerSpeed integer
+---@param assemblersNeeded integer
+---@param productAmount integer
+---@param amountInRecipe integer
 local function printResult(amountOutput, itemCraftingSpeed, 
-assemblerSpeed, assemblersNeeded)
+assemblerSpeed, assemblersNeeded, productAmount, amountInRecipe)
 
     print("With the following: \n")
+    print("Product Amount Needed: "..productAmount.."\n")
+
+    print("Amount in Recipe: "..amountInRecipe)
     print("Amount output: "..amountOutput)
     print("Item Crafting Speed: "..itemCraftingSpeed)
     print("Assembler Speed: "..assemblerSpeed)
 
-    print("You need "..assemblersNeeded.." assemblers to equal to one")
+    print("You need "..assemblersNeeded.." assemblers to equal to 1/s")
 end
 
 
 function AssemblersNeeded()
-    local assemblersNeeded, amountOutput, itemCraftingSpeed, assemblerSpeed
+    local assemblersNeeded, amountOutput, itemCraftingSpeed, assemblerSpeed, productAmount,
+    amountInRecipe
     local quit = false
     repeat
         os.execute("clear")
 
         repeat
-            print("Amount Output: ")
+            print("Amount Output for each cycle: ")
             amountOutput = io.read("n")
         until (amountOutput > 0)
 
@@ -36,9 +47,23 @@ function AssemblersNeeded()
         until(assemblerSpeed > 0.0)
 
         os.execute("clear")
-        assemblersNeeded = math.ceil(((1.0/amountOutput) * itemCraftingSpeed) / assemblerSpeed)
+        repeat
+            print("Product Amount Needed: ")
+            productAmount = io.read("n")
+        until (productAmount > 0)
 
-        printResult(amountOutput, itemCraftingSpeed, assemblerSpeed, assemblersNeeded)
+        os.execute("clear")
+        repeat
+            print("Amount in Recipe: ")
+            amountInRecipe = io.read("n")
+        until (amountInRecipe > 0)
+
+        os.execute("clear")
+        assemblersNeeded = math.ceil(((amountInRecipe * productAmount) /amountOutput) * 
+        (itemCraftingSpeed / assemblerSpeed))
+
+        printResult(amountOutput, itemCraftingSpeed, assemblerSpeed, assemblersNeeded, 
+        productAmount, amountInRecipe)
 
         quit = ChoiceToQuit()
 
